15a2801578bf0ae1dfc1f74d145ac373babc9257
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2023-01-29 22:12:04 +0200

    changed contact page theme

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,59 +2,64 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-      <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
+    <div
+      id="contact"
+      className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 bg-[#001b5e]"
+    >
+      <h1 className="py-4 text-4xl font-bold text-center text-white">
         Contact
       </h1>
       <form
         action=""
         method="POST"
         encType="multipart/form-data"
-        className="flex flex-col items-center w-2/4 rounded-md mx-auto"
+        className="flex flex-col items-center md:w-2/4 w-full rounded-md mx-auto"
       >
         <div className="grid md:grid-cols-2 gap-4 py-2 w-full">
           <div className="flex flex-col w-full">
-            <label className="uppercase text-sm py-2">Name</label>
+            <label className="uppercase text-white text-sm py-2">Name</label>
             <input
-              className="border-2 rounded-lg p-2 flex border-gray-300"
+              className="border-2 rounded-lg p-2 flex bg-slate-300"
               type="text"
               name="name"
             />
           </div>
           <div className="flex flex-col">
-            <label className="uppercase text-sm py-2">Phone Number</label>
+            <label className="uppercase text-white text-sm py-2">
+              Phone Number
+            </label>
             <input
-              className="border-2 rounded-lg p-2 flex border-gray-300"
+              className="border-2 rounded-lg p-2 flex bg-slate-300"
               type="text"
               name="phone"
             />
           </div>
         </div>
         <div className="flex flex-col w-full ">
-          <label className="uppercase text-sm py-1">Email</label>
+          <label className="uppercase text-white text-sm py-1">Email</label>
           <input
-            className="border-2 rounded-lg p-2 flex border-gray-300"
+            className="border-2 rounded-lg p-2 flex bg-slate-300"
             type="email"
             name="email"
           />
         </div>
         <div className="flex flex-col w-full ">
-          <label className="uppercase text-sm py-2">Subject</label>
+          <label className="uppercase text-white text-sm py-2">Subject</label>
           <input
-            className="border-2 rounded-lg p-2 flex border-gray-300"
+            className="border-2 rounded-lg p-2 flex bg-slate-300"
             type="text"
             name="subject"
           />
         </div>
         <div className="flex flex-col w-full ">
-          <label className="uppercase text-sm py-2">Message</label>
+          <label className="uppercase text-white text-sm py-2">Message</label>
           <textarea
-            className="border-2 rounded-lg p-2 flex border-gray-300"
+            className="border-2 rounded-lg p-2 flex bg-slate-300"
             rows="5"
             name="message"
           ></textarea>
         </div>
-        <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4">
+        <button className="bg-white text-[#001b5e] rounded-lg mt-4 w-full p-4">
           Send Message
         </button>
       </form>
