08ee3b2b5b84f256f739b9a0f26585bb4c12031b
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2023-06-14 10:27:31 +0000

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import bhelperImg from "../assets/bhelper.jpg";
+import labinvImg from "../assets/labinv.jpg";
+import calcImg from "../assets/Calculators.jpg";
+import twitterImg from "../assets/twitter_screenshot.jpg"
+import emechImg from "../assets/e-mechanic.jpg";
+
+export {bhelperImg, labinvImg, calcImg, twitterImg, emechImg};
